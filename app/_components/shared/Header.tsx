48b6d1942ce12906371319ffb91714e4ccc29c70
Peter Dinis <peterdinis0@pobox.sk>
2024-06-21 19:24:54 +0200

    feat: Pilot design for cards

--- app/_components/shared/Header.tsx
+++ app/_components/shared/Header.tsx
@@ -6,7 +6,7 @@ interface IHeaderProps {
 
 const Header: FC<IHeaderProps> = ({text}: IHeaderProps) => {
     return (
-        <h2 className="scroll-m-20 prose prose-h2: text-2xl font-extrabold tracking-tight lg:text-5xl">
+        <h2 className="scroll-m-20 prose prose-h2: dark:text-white text-2xl font-extrabold tracking-tight lg:text-5xl">
             {text}
         </h2>
     )
