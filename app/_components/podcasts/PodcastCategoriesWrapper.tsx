91938829b651fb9b6805797aad60c1af0c64345e
Peter Dinis <peterdinis0@pobox.sk>
2024-06-22 21:18:40 +0200

    FOO

--- app/_components/podcasts/PodcastCategoriesWrapper.tsx
+++ app/_components/podcasts/PodcastCategoriesWrapper.tsx
@@ -1,10 +1,11 @@
 import { FC } from "react";
 import DefaultLayout from "../shared/layouts/DefaultLayout";
+import Header from "../shared/Header";
 
 const PodcastCategoriesWrapper: FC = () => {
     return (
         <DefaultLayout>
-        rrr
+            <Header text="Categories" />
         </DefaultLayout>
     )
 }
