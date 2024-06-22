c8298c7cf12bac83a1578c793118bdb14a1ee85d
Peter Dinis <peterdinis0@pobox.sk>
2024-06-22 21:25:37 +0200

    feat: Add design for podcast categories

--- app/_components/podcasts/PodcastCategoriesWrapper.tsx
+++ app/_components/podcasts/PodcastCategoriesWrapper.tsx
@@ -1,11 +1,13 @@
 import { FC } from "react";
 import DefaultLayout from "../shared/layouts/DefaultLayout";
 import Header from "../shared/Header";
+import CategoriesLists from "./categories/CategoriesLists";
 
 const PodcastCategoriesWrapper: FC = () => {
     return (
         <DefaultLayout>
             <Header text="Categories" />
+            <CategoriesLists />
         </DefaultLayout>
     )
 }
---
+++ app/_components/podcasts/categories/CategoriesLists.tsx
@@ -0,0 +1,9 @@
+import { HoverEffect } from "@/components/ui/card-hover-effect";
+import { FC } from "react";
+import { projects } from "./data";
+
+const CategoriesLists: FC = () => {
+    return <HoverEffect items={projects} />
+}
+
+export default CategoriesLists;
