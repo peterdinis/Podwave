ae2a2e0550b6066e7923bc187942735787e41d7f
Peter Dinis <peterdinis0@pobox.sk>
2024-06-22 17:29:35 +0200

    FOO

---
+++ app/_components/podcasts/CreateNewPodcastForm.tsx
@@ -0,0 +1,14 @@
+import { Form } from '@/components/ui/form';
+import { FC } from 'react';
+
+const CreateNewPodcastForm: FC = () => {
+    return (
+        <section className='mt-10 flex flex-col'>
+            <form className='mt-12 flex w-full flex-col'>
+                
+            </form>
+        </section>
+    );
+};
+
+export default CreateNewPodcastForm;
--- app/_components/podcasts/CreateNewPodcastWrapper.tsx
+++ app/_components/podcasts/CreateNewPodcastWrapper.tsx
@@ -1,11 +1,13 @@
 import { FC } from "react";
 import DefaultLayout from "../shared/layouts/DefaultLayout";
 import Header from "../shared/Header";
+import CreateNewPodcastForm from "./CreateNewPodcastForm";
 
 const CreateNewPodcastWrapper: FC = () => {
     return (
         <DefaultLayout>
             <Header text="Create new podcast" />
+            <CreateNewPodcastForm />
         </DefaultLayout>
     )
 }
