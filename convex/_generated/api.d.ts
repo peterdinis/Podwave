d5b97010018d13d6647f64f5c05b16e53e8bf02b
Peter Dinis <peterdinis0@pobox.sk>
2024-06-22 15:46:48 +0200

    ffoooff

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -15,7 +15,6 @@ import type {
   FunctionReference,
 } from "convex/server";
 import type * as http from "../http.js";
-import type * as users from "../users.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -27,7 +26,6 @@ import type * as users from "../users.js";
  */
 declare const fullApi: ApiFromModules<{
   http: typeof http;
-  users: typeof users;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
