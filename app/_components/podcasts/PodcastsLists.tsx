7d2a15182bd6aa2e2f5bec7f0284a5939d814451
Peter Dinis <peterdinis0@pobox.sk>
2024-06-23 14:33:39 +0200

    fix: Fixing carousel bug

--- app/_components/podcasts/PodcastsLists.tsx
+++ app/_components/podcasts/PodcastsLists.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { FC } from 'react';
+import { FC, useState, useEffect, SetStateAction } from 'react';
 import { motion } from 'framer-motion';
 import Header from '../shared/Header';
 import {
@@ -20,27 +20,52 @@ const slideAnimation = {
 };
 
 const PodcastsLists: FC = () => {
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const totalSlides = 12; // Total number of slides
+    const visibleSlidesLg = 4;
+    const visibleSlidesMd = 2;
+    const visibleSlidesSm = 1;
+
+    const handleSlideChange = (e: { target: { value: SetStateAction<number>; }; }) => {
+        setCurrentSlide(e.target.value);
+    };
+
+    useEffect(() => {
+        const carousels = document.querySelectorAll('.carousel');
+        carousels.forEach((carousel) => {
+            const slider = carousel.querySelector('.pure-react-carousel__slider');
+            slider?.addEventListener('scroll', (e) => {
+                const newIndex = Math.round(
+                    e.currentTarget.scrollLeft / e.currentTarget.clientWidth
+                );
+                setCurrentSlide(newIndex);
+            });
+        });
+    }, []);
+
     return (
         <>
             <Header text='Top podcasts' />
             <div className='container mx-auto'>
                 <div className='flex h-full w-full items-center justify-center px-4 py-24 sm:py-8'>
                     <CarouselProvider
-                        className='hidden lg:block'
+                        className='hidden lg:block carousel'
                         naturalSlideWidth={100}
                         isIntrinsicHeight={true}
-                        totalSlides={12}
-                        visibleSlides={4}
+                        totalSlides={totalSlides}
+                        visibleSlides={visibleSlidesLg}
                         step={1}
-                        infinite={true}
-                        naturalSlideHeight={0}
+                        infinite={false}
+                        currentSlide={currentSlide}
+                        onChange={handleSlideChange}
                     >
                         <div className='relative flex w-full items-center justify-center'>
                             <ButtonBack
                                 role='button'
                                 aria-label='slide backward'
-                                className='absolute left-0 z-30 ml-8 cursor-pointer focus:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2'
+                                className={`absolute left-0 z-30 ml-8 cursor-pointer focus:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 ${currentSlide === 0 ? 'opacity-50 cursor-not-allowed' : ''}`}
                                 id='prev'
+                                disabled={currentSlide === 0}
                             >
                                 <ArrowLeft />
                             </ButtonBack>
@@ -50,7 +75,7 @@ const PodcastsLists: FC = () => {
                                         id='slider'
                                         className='flex h-full items-center justify-start gap-14 transition duration-700 ease-out md:gap-6 lg:gap-8'
                                     >
-                                        {[...Array(8)].map((_, index) => (
+                                        {[...Array(totalSlides)].map((_, index) => (
                                             <Slide index={index} key={index}>
                                                 <motion.div
                                                     initial="hidden"
@@ -69,8 +94,9 @@ const PodcastsLists: FC = () => {
                             <ButtonNext
                                 role='button'
                                 aria-label='slide forward'
-                                className='absolute right-0 z-30 mr-8 focus:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2'
+                                className={`absolute right-0 z-30 mr-8 focus:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 ${currentSlide === totalSlides - visibleSlidesLg ? 'opacity-50 cursor-not-allowed' : ''}`}
                                 id='next'
+                                disabled={currentSlide === totalSlides - visibleSlidesLg}
                             >
                                 <ArrowRight />
                             </ButtonNext>
@@ -80,20 +106,23 @@ const PodcastsLists: FC = () => {
                     {/* Carousel for tablet and medium size devices */}
                     <CarouselProvider
                         naturalSlideHeight={100}
-                        className='hidden md:block lg:hidden'
+                        className='hidden md:block lg:hidden carousel'
                         naturalSlideWidth={100}
                         isIntrinsicHeight={true}
-                        totalSlides={12}
-                        visibleSlides={2}
+                        totalSlides={totalSlides}
+                        visibleSlides={visibleSlidesMd}
                         step={1}
-                        infinite={true}
+                        infinite={false}
+                        currentSlide={currentSlide}
+                        onChange={handleSlideChange}
                     >
                         <div className='relative flex w-full items-center justify-center'>
                             <ButtonBack
                                 role='button'
                                 aria-label='slide backward'
-                                className='absolute left-0 z-30 ml-8 cursor-pointer focus:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2'
+                                className={`absolute left-0 z-30 ml-8 cursor-pointer focus:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 ${currentSlide === 0 ? 'opacity-50 cursor-not-allowed' : ''}`}
                                 id='prev'
+                                disabled={currentSlide === 0}
                             >
                                 <ArrowLeft />
                             </ButtonBack>
@@ -103,7 +132,7 @@ const PodcastsLists: FC = () => {
                                         id='slider'
                                         className='flex h-full items-center justify-start gap-14 transition duration-700 ease-out md:gap-6 lg:gap-8'
                                     >
-                                        {[...Array(4)].map((_, index) => (
+                                        {[...Array(totalSlides)].map((_, index) => (
                                             <Slide index={index} key={index}>
                                                 <motion.div
                                                     initial="hidden"
@@ -122,8 +151,9 @@ const PodcastsLists: FC = () => {
                             <ButtonNext
                                 role='button'
                                 aria-label='slide forward'
-                                className='absolute right-0 z-30 mr-8 focus:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2'
+                                className={`absolute right-0 z-30 mr-8 focus:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 ${currentSlide === totalSlides - visibleSlidesMd ? 'opacity-50 cursor-not-allowed' : ''}`}
                                 id='next'
+                                disabled={currentSlide === totalSlides - visibleSlidesMd}
                             >
                                 <ArrowRight />
                             </ButtonNext>
@@ -132,21 +162,23 @@ const PodcastsLists: FC = () => {
 
                     {/* Carousel for mobile and Small size Devices */}
                     <CarouselProvider
-                        className='block md:hidden'
+                        className='block md:hidden carousel'
                         naturalSlideWidth={100}
                         isIntrinsicHeight={true}
-                        totalSlides={12}
-                        visibleSlides={1}
+                        totalSlides={totalSlides}
+                        visibleSlides={visibleSlidesSm}
                         step={1}
-                        infinite={true}
-                        naturalSlideHeight={0}
+                        infinite={false}
+                        currentSlide={currentSlide}
+                        onChange={handleSlideChange}
                     >
                         <div className='relative flex w-full items-center justify-center'>
                             <ButtonBack
                                 role='button'
                                 aria-label='slide backward'
-                                className='absolute left-0 z-30 ml-8 cursor-pointer focus:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2'
+                                className={`absolute left-0 z-30 ml-8 cursor-pointer focus:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 ${currentSlide === 0 ? 'opacity-50 cursor-not-allowed' : ''}`}
                                 id='prev'
+                                disabled={currentSlide === 0}
                             >
                                 <ArrowLeft />
                             </ButtonBack>
@@ -156,7 +188,7 @@ const PodcastsLists: FC = () => {
                                         id='slider'
                                         className='flex h-full w-full items-center justify-start transition duration-700 ease-out md:gap-6 lg:gap-8'
                                     >
-                                        {[...Array(2)].map((_, index) => (
+                                        {[...Array(totalSlides)].map((_, index) => (
                                             <Slide index={index} key={index}>
                                                 <motion.div
                                                     initial="hidden"
@@ -175,8 +207,9 @@ const PodcastsLists: FC = () => {
                             <ButtonNext
                                 role='button'
                                 aria-label='slide forward'
-                                className='absolute right-0 z-30 mr-8 focus:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2'
+                                className={`absolute right-0 z-30 mr-8 focus:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 ${currentSlide === totalSlides - visibleSlidesSm ? 'opacity-50 cursor-not-allowed' : ''}`}
                                 id='next'
+                                disabled={currentSlide === totalSlides - visibleSlidesSm}
                             >
                                 <ArrowRight />
                             </ButtonNext>
