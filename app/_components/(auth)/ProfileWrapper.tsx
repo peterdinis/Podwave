f49dfa04c772290536e27060f5aba80be95fa711
Peter Dinis <peterdinis0@pobox.sk>
2024-06-23 20:37:28 +0200

    feat: Updating profile wrapper

--- app/_components/(auth)/ProfileWrapper.tsx
+++ app/_components/(auth)/ProfileWrapper.tsx
@@ -1,20 +1,17 @@
 'use client';
 
-import { FC, ReactNode } from 'react';
+import { FC} from 'react';
 import DefaultLayout from '../shared/layouts/DefaultLayout';
 import Header from '../shared/Header';
 import { useUser } from '@clerk/nextjs';
-import ProfilePodcasts from '../podcasts/profile/ProfilePodcasts';
 
 const ProfileWrapper: FC = () => {
     const { user } = useUser();
     return (
         <DefaultLayout>
-            <Header text='My Profile' />
-            <h2 className='prose-h2: prose mt-5 p-4 text-2xl font-bold'>
-                {user?.fullName} Welcome
-            </h2>
-            <ProfilePodcasts />
+            <Header text={user?.fullName as unknown as string} />
+            <h2 className='mt-5 p-3 text-xl prose prose-p: dark:text-white font-bold'>My favorite podcasts</h2>
+
         </DefaultLayout>
     );
 };
