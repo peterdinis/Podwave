77c91657c16664f3a56d7d30f17a5ea38178c3ea
Peter Dinis <peterdinis0@pobox.sk>
2024-07-02 17:36:01 +0200

    fix: Update typing

--- app/_components/podcasts/PodcastCard.tsx
+++ app/_components/podcasts/PodcastCard.tsx
@@ -1,33 +1,12 @@
 import { FC } from 'react';
 import { Button } from '@/components/ui/button';
 import { Card, CardHeader, CardContent, CardTitle } from '@/components/ui/card';
-import { Id } from '@/convex/_generated/dataModel';
 import Image from 'next/image';
 import Link from 'next/link';
-
-interface Podcast {
-    audioDuration: number;
-    audioStorageId: any;
-    audioUrl: string;
-    author: string;
-    authorId: string;
-    authorImageUrl: string;
-    categoryId: string;
-    imagePrompt: string;
-    imageStorageId: string;
-    imageUrl: string;
-    podcastDescription: string;
-    podcastTitle: string;
-    user: string;
-    views: number;
-    voicePrompt: string;
-    voiceType: string;
-    _creationTime: number;
-    _id: Id<"podcasts">
-}
+import { PodcastType } from '@/app/_types/podcastTypes';
 
 interface PodcastCardProps {
-    podcast: Podcast | any;
+    podcast: PodcastType
 }
 
 const PodcastCard: FC<PodcastCardProps> = ({ podcast }) => {
--- app/_types/podcastTypes.ts
+++ app/_types/podcastTypes.ts
@@ -10,3 +10,26 @@ export interface GeneratePodcastProps {
     setVoicePrompt: Dispatch<SetStateAction<string>>;
     setAudioDuration: Dispatch<SetStateAction<number>>;
 }
+
+interface Podcast {
+    audioDuration: number;
+    audioStorageId: string;
+    audioUrl: string;
+    author: string;
+    authorId: string;
+    authorImageUrl: string;
+    categoryId: string;
+    imagePrompt: string;
+    imageStorageId: string;
+    imageUrl: string;
+    podcastDescription: string;
+    podcastTitle: string;
+    user: string;
+    views: number;
+    voicePrompt: string;
+    voiceType: string;
+    _creationTime: number;
+    _id: Id<"podcasts">
+}
+
+export type PodcastType = Partial<Podcast>;
