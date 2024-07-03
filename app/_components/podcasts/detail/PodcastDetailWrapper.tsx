b28da5cc8778dfba7ded29f7bdfc36599d59a859
Peter Dinis <peterdinis0@pobox.sk>
2024-07-03 21:56:13 +0200

    FOO

--- app/_components/podcasts/detail/PodcastDetailWrapper.tsx
+++ app/_components/podcasts/detail/PodcastDetailWrapper.tsx
@@ -6,7 +6,9 @@ import { useUser } from '@clerk/nextjs';
 import { useQuery } from 'convex/react';
 import { Loader2 } from 'lucide-react';
 import { useParams, useRouter } from 'next/navigation';
+import Image from 'next/image';
 import { FC } from 'react';
+import EmptyState from '../../shared/EmptyState';
 
 const PodcastDetailWrapper: FC = () => {
     const { id } = useParams();
@@ -25,68 +27,88 @@ const PodcastDetailWrapper: FC = () => {
     if (!similarPodcasts || !data)
         return <Loader2 className='h-8 w-8 animate-spin' />;
     return (
-        <section className="flex w-full flex-col">
-      <header className="mt-9 flex items-center justify-between">
-        <h1 className="text-20 font-bold text-white-1">
-          Currenty Playing
-        </h1>
-        <figure className="flex gap-3">
-          <Image
-            src="/icons/headphone.svg"
-            width={24}
-            height={24}
-            alt="headphone"
-          />
-          <h2 className="text-16 font-bold text-white-1">{podcast?.views}</h2>
-        </figure>
-      </header>
+        <section className='flex w-full flex-col'>
+            <header className='mt-9 flex items-center justify-between'>
+                <h1 className='text-20 text-white-1 font-bold'>
+                    Currenty Playing
+                </h1>
+                <figure className='flex gap-3'>
+                    <Image
+                        src='/icons/headphone.svg'
+                        width={24}
+                        height={24}
+                        alt='headphone'
+                    />
+                    <h2 className='text-16 text-white-1 font-bold'>
+                        {data?.views}
+                    </h2>
+                </figure>
+            </header>
 
-      <PodcastDetailPlayer 
-        isOwner={isOwner}
-        podcastId={podcast._id}
-        {...podcast}
-      />
-
-      <p className="text-white-2 text-16 pb-8 pt-[45px] font-medium max-md:text-center">{podcast?.podcastDescription}</p>
-
-      <div className="flex flex-col gap-8">
-        <div className='flex flex-col gap-4'>
-          <h1 className='text-18 font-bold text-white-1'>Transcription</h1>
-          <p className="text-16 font-medium text-white-2">{podcast?.voicePrompt}</p>
-        </div>
-        <div className='flex flex-col gap-4'>
-          <h1 className='text-18 font-bold text-white-1'>Thumbnail Prompt</h1>
-          <p className="text-16 font-medium text-white-2">{podcast?.imagePrompt}</p>
-        </div>
-      </div>
-      <section className="mt-8 flex flex-col gap-5">
-        <h1 className="text-20 font-bold text-white-1">Similar Podcasts</h1>
-
-        {similarPodcasts && similarPodcasts.length > 0 ? (
-          <div className="podcast_grid">
-            {similarPodcasts?.map(({ _id, podcastTitle, podcastDescription, imageUrl }) => (
-              <PodcastCard 
-                key={_id}
-                imgUrl={imageUrl as string}
-                title={podcastTitle}
-                description={podcastDescription}
-                podcastId={_id}
-              />
-            ))}
-          </div>
-        ) : (
-          <> 
-            <EmptyState 
-              title="No similar podcasts found"
-              buttonLink="/discover"
-              buttonText="Discover more podcasts"
+            <PodcastDetailPlayer
+                isOwner={isOwner}
+                podcastId={podcast._id}
+                {...podcast}
             />
-          </>
-        )}
-      </section>
 
-    </section>
-    )
+            <p className='text-white-2 text-16 pb-8 pt-[45px] font-medium max-md:text-center'>
+                {data?.podcastDescription}
+            </p>
+
+            <div className='flex flex-col gap-8'>
+                <div className='flex flex-col gap-4'>
+                    <h1 className='text-18 text-white-1 font-bold'>
+                        Transcription
+                    </h1>
+                    <p className='text-16 text-white-2 font-medium'>
+                        {data?.voicePrompt}
+                    </p>
+                </div>
+                <div className='flex flex-col gap-4'>
+                    <h1 className='text-18 text-white-1 font-bold'>
+                        Thumbnail Prompt
+                    </h1>
+                    <p className='text-16 text-white-2 font-medium'>
+                        {data?.imagePrompt}
+                    </p>
+                </div>
+            </div>
+            <section className='mt-8 flex flex-col gap-5'>
+                <h1 className='text-20 text-white-1 font-bold'>
+                    Similar Podcasts
+                </h1>
+
+                {similarPodcasts && similarPodcasts.length > 0 ? (
+                    <div className='podcast_grid'>
+                        {similarPodcasts?.map(
+                            ({
+                                _id,
+                                podcastTitle,
+                                podcastDescription,
+                                imageUrl,
+                            }) => (
+                                <PodcastCard
+                                    key={_id}
+                                    imgUrl={imageUrl as string}
+                                    title={podcastTitle}
+                                    description={podcastDescription}
+                                    podcastId={_id}
+                                />
+                            ),
+                        )}
+                    </div>
+                ) : (
+                    <>
+                        <EmptyState
+                            title='No similar podcasts found'
+                            buttonLink='/discover'
+                            buttonText='Discover more podcasts'
+                        />
+                    </>
+                )}
+            </section>
+        </section>
+    );
 };
 
 export default PodcastDetailWrapper;
