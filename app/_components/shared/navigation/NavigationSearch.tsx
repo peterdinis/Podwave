e3f909bff041d82ec978e7452194c7d455aa15bf
Peter Dinis <peterdinis0@pobox.sk>
2024-07-07 17:50:26 +0200

    feat: Start adding search logic

--- app/_components/shared/navigation/NavigationSearch.tsx
+++ app/_components/shared/navigation/NavigationSearch.tsx
@@ -1,8 +1,9 @@
+import { Input } from '@/components/ui/input';
 import { FC } from 'react';
 
 const NavigationSearch: FC = () => {
     return (
-        <input
+        <Input
             className='focus:shadow-outline w-9/12 rounded-full bg-gray-100 py-4 pl-4 text-xs font-bold uppercase leading-tight text-gray-700 focus:outline-none lg:text-sm'
             type='text'
             placeholder='Search'
