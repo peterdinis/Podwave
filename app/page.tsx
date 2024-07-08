be49a3591886a4f50d75dd8c384f302e5e46f37f
Peter Dinis <peterdinis0@pobox.sk>
2024-07-08 14:49:08 +0200

    FOO

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,7 @@ import { NextPage } from 'next';
 import HomeWrapper from './_components/home/HomeWrapper';
 
 const Homepage: NextPage = () => {
-    return (
-        <>
-            <HomeWrapper />
-        </>
-    );
+    return <HomeWrapper />
 };
 
 export default Homepage;
