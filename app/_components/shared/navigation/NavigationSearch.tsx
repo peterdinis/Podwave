b2e17920ac85bb42febacaa79f2b31a7fd618d4c
Peter Dinis <peterdinis0@pobox.sk>
2024-07-11 17:45:34 +0200

    feat: Start building logic for global search

--- app/_components/shared/navigation/NavigationSearch.tsx
+++ app/_components/shared/navigation/NavigationSearch.tsx
@@ -1,11 +1,25 @@
+"use client"
+
+import { FC, useState, ChangeEvent } from 'react';
+import { usePathname } from 'next/navigation'
 import { Input } from '@/components/ui/input';
-import { FC } from 'react';
 
 const NavigationSearch: FC = () => {
+    const pathname = usePathname()
+    const [searchQuery, setSearchQuery] = useState<string>('');
+
+    const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
+        setSearchQuery(event.target.value);
+        if (pathname === '/') {
+            console.log('Vyhľadávam v podcasts: ', event.target.value);
+        } else if (pathname === '/categories') {
+            console.log('Vyhľadávam v kategóriách: ', event.target.value);
+        }
+    };
+
     return (
         <Input
-            className='
-                w-full
+         className=' w-full
                 sm:w-10/12
                 md:w-8/12
                 lg:w-6/12
@@ -21,10 +35,11 @@ const NavigationSearch: FC = () => {
                 text-gray-700
                 focus:outline-none
                 focus:shadow-outline
-                lg:text-sm
-            '
+                lg:text-sm'
             type='text'
             placeholder='Search'
+            value={searchQuery}
+            onChange={handleSearch}
         />
     );
 };
