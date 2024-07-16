ee3bc0605a35f6d65238bb27680e58bd8ebe4e64
Peter Dinis <peterdinis0@pobox.sk>
2024-07-16 15:11:08 +0200

    FOO

--- app/_components/podcasts/categories/CategoryInfo.tsx
+++ app/_components/podcasts/categories/CategoryInfo.tsx
@@ -1,24 +1,36 @@
 'use client';
 
 import { FC } from 'react';
-import { useParams } from 'next/navigation';
+import { useParams, useRouter } from 'next/navigation';
 import { useQuery } from 'convex/react';
 import { api } from '@/convex/_generated/api';
 import { Id } from '@/convex/_generated/dataModel';
 import DefaultLayout from '../../shared/layouts/DefaultLayout';
 import Header from '../../shared/Header';
-import CategoryPodcastTable from './CategoryPodcastTable';
-import { Podcast } from '@/app/_types/podcastTypes';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardFooter } from '@/components/ui/card';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from '@/components/ui/table';
+import Image from 'next/image';
+import { Tabs, TabsContent } from '@/components/ui/tabs';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { format } from 'date-fns';
+import ProfilePodcastsPagination from '../profile/ProfilePodcastsPagination';
+import { PodcastType } from '@/app/_types/podcastTypes';
 
 const CategoryInfo: FC = () => {
     const { id } = useParams();
+    const router = useRouter();
 
     const data = useQuery(api.categories.getCategoryById, {
         categoryId: id[0] as unknown as Id<'categories'>,
     });
-
-    console.log("D", data);
-
     return (
         <DefaultLayout>
             <Header text={data?.category?.categoryName as unknown as string} />
@@ -27,7 +39,93 @@ const CategoryInfo: FC = () => {
                 Podcasts from this category
             </div>
             <div className='mt-3'>
-                <CategoryPodcastTable podcastData={data?.podcasts as unknown as Podcast} />
+                <ScrollArea>
+                    <main className='grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8'>
+                        <Tabs defaultValue='all'>
+                            <TabsContent value='all'>
+                                <Card x-chunk='dashboard-06-chunk-0'>
+                                    <CardContent>
+                                        <Table>
+                                            <TableHeader>
+                                                <TableRow>
+                                                    <TableHead className='hidden w-[100px] sm:table-cell'>
+                                                        Image
+                                                    </TableHead>
+                                                    <TableHead>Name</TableHead>
+                                                    <TableHead>
+                                                        Description
+                                                    </TableHead>
+                                                    <TableHead className='hidden md:table-cell'>
+                                                        Added
+                                                    </TableHead>
+                                                    <TableHead>
+                                                        Actions
+                                                    </TableHead>
+                                                </TableRow>
+                                            </TableHeader>
+                                            <TableBody>
+                                                {data?.podcasts &&
+                                                    data.podcasts.map(
+                                                        (item: PodcastType) => {
+                                                            return (
+                                                                <>
+                                                                    <TableRow>
+                                                                        <TableCell className='hidden sm:table-cell'>
+                                                                            <Image
+                                                                                alt={
+                                                                                    item.podcastTitle as unknown as string
+                                                                                }
+                                                                                className='aspect-square rounded-md object-cover'
+                                                                                height='64'
+                                                                                src={
+                                                                                    item.imageUrl as unknown as string
+                                                                                }
+                                                                                width='64'
+                                                                            />
+                                                                        </TableCell>
+                                                                        <TableCell className='font-medium'>
+                                                                            {
+                                                                                item.podcastTitle
+                                                                            }
+                                                                        </TableCell>
+                                                                        <TableCell className='hidden md:table-cell'>
+                                                                            {
+                                                                                item.podcastDescription
+                                                                            }
+                                                                        </TableCell>
+                                                                        <TableCell className='hidden md:table-cell'>
+                                                                            {format(
+                                                                                new Date(),
+                                                                                'yyyy-MM-dd',
+                                                                            )}
+                                                                        </TableCell>
+                                                                        <TableCell>
+                                                                            <Button
+                                                                                onClick={() => {
+                                                                                    router.push(
+                                                                                        `/podcasts/${item._id}`,
+                                                                                    );
+                                                                                }}
+                                                                            >
+                                                                                Detail
+                                                                            </Button>
+                                                                        </TableCell>
+                                                                    </TableRow>
+                                                                </>
+                                                            );
+                                                        },
+                                                    )}
+                                            </TableBody>
+                                        </Table>
+                                    </CardContent>
+                                    <CardFooter>
+                                        <ProfilePodcastsPagination />
+                                    </CardFooter>
+                                </Card>
+                            </TabsContent>
+                        </Tabs>
+                    </main>
+                </ScrollArea>
             </div>
         </DefaultLayout>
     );
